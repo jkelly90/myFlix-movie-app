1bdb56f81cb2e9d6571408535ba8e992b0f07a28
jkelly90 <jlfkelly@gmail.com>
2019-09-15 16:26:41 +0200

    added morgan and express.static

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
 
 //json object
 let topMovies = [ {
@@ -53,4 +57,8 @@ app.get('/', function(req, res){
     res.send('Welcome to my movie app!')
 });
 
+
+app.use(express.static('public'));
+
+app.listen(8080);
 
