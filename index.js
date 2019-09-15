48c497a3bfe2b2426974818847119414d3e72a1e
jkelly90 <jlfkelly@gmail.com>
2019-09-15 15:45:50 +0200

    added list of movies

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 const express = require('express');
 const app = express();
+
+//json object
+let movies = [ {
+    title: 'Lost in Translation',
+    director: 'Sofia Coppola'
+},
+{
+    title: 'Midnight in Paris',
+    director: 'Woody Allen'
+},
+{
+    title: 'Wings of Desire',
+    director: 'Wim Wenders'
+},
+{
+    title: 'Dances with Wolves ',
+    director: 'Kevin Costner'
+},
+{
+    title: 'Spirited Away',
+    director: 'Hayao Miyazaki'
+},
+{
+    title: 'Coco',
+    director: 'Lee Unkirch'
+},
+{
+    title: 'The Lives of Others',
+    director: 'Florian Henckel von Donnersmarck'
+},
+{
+    title: 'The Hunt',
+    director: 'Thomas Vinterberg'
+},
+{
+    title: '2001: A Space Odyssey',
+    director: 'Stanley Kubrick'
+},
+{
+    title: 'Your Name',
+    director: 'Makoto Shinkai'
+}
+]
+
