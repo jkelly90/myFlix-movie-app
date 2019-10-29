646adcc4cab8e5d1128256d465211c054f689ff6
jkelly90 <jlfkelly@gmail.com>
2019-10-29 12:56:00 +0100

    added authentication code

---
+++ passport.js
@@ -0,0 +1,7 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+
+
