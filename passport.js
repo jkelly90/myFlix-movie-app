8c69f153b1bde0cbd0e9cb20c30c02727527c86c
jkelly90 <jlfkelly@gmail.com>
2019-10-29 14:09:07 +0100

    fixed typo

--- passport.js
+++ passport.js
@@ -30,7 +30,7 @@ passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'your_jwt_secret'
 },  (jwtPayload, callback) => {
-    return Users.findById(jwtPayLoad._id)
+    return Users.findById(jwtPayload._id)
     .then((user) => {
         return callback(null, user);
     });
