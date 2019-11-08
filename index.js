53232f96f399c43822baa2042f1ce7e836eaa21b
jkelly90 <jlfkelly@gmail.com>
2019-11-08 10:32:35 +0100

    enable cors for local react app

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ let auth = require('./auth')(app);
 const cors = require('cors');
 app.use(cors());
 
+var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://testsite.com', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 //Welcome message
 app.get("/", (req, res) => {
   res.send('Welcome to my movie app!')
