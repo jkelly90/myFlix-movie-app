dd59b6e837801567394f40ed0a9a23d34d72bd91
jkelly90 <jlfkelly@gmail.com>
2019-11-15 18:09:46 +0100

    typo fix

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -8,20 +8,20 @@ export function LoginView(props) {
         e, preventDefault();
         console.log(username, password);
         props.onLoggedIn(username);
-    }
-};
+    };
 
-return (
-    <form>
-        <label>
-            Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        </label>
-        <label>
-            Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-        <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
-);
+
+    return (
+        <form>
+            <label>
+                Username:
+                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="button" onClick={handleSubmit}>Submit</button>
+        </form>
+    );
 }
