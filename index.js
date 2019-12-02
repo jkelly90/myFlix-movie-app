3383e75796321af87c5d3974d478aff9dc38d592
jkelly90 <jlfkelly@gmail.com>
2019-12-02 15:27:43 +0100

    changed allowed origins

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let auth = require('./auth')(app);
 const cors = require('cors');
 app.use(cors());
 
-var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://testsite.com', "http://localhost:1234"];
+var allowedOrigins = ["*"/*'http://localhost:8080', 'http://localhost:3000', 'http://testsite.com', "http://localhost:1234"*/];
 
 app.use(cors({
   origin: function (origin, callback) {
