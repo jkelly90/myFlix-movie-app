ce2c69cff004c2acc39a4628807d83656cbb9811
jkelly90 <jlfkelly@gmail.com>
2020-01-09 14:24:16 +0100

    added access-control--allow-origin function

--- index.js
+++ index.js
@@ -217,6 +217,12 @@ app.delete("/users/:Username", passport.authenticate('jwt', { session: false }),
     });
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
+});
+
 app.use(express.static('public'));
 
 app.use(function (err, req, res, next) {
